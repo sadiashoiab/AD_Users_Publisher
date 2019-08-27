0735ea6060a4525f2058414f330b248fbb0bb7e3
Jamie Wallingford <jamie.wallingford@capstonec.com>
2019-08-27 14:09:21 -0500

    adding in test project

---
+++ AD_Users_Publisher/AD_Users_Publisher.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AD_Users_Publisher.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ AD_Users_Publisher/AD_Users_Publisher/Controllers/PublishController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AD_Users_Publisher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishController : ControllerBase
+    {
+        // GET api/publish/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+    }
+}
+
+// https://prod-24.centralus.logic.azure.com:443/workflows/27dfb5ebbe844a1595d1a7ec0bd2575a/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=wVh5Ycww6R2qqY-ck_w2iWSG7Ij3ASTvS3FmeBpv-_w
