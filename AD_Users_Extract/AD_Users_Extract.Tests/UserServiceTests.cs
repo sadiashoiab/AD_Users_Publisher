ed87bd6757d70f177dfae383aeea8380e601833c
Jamie Wallingford <jamie.wallingford@capstonec.com>
2019-08-27 14:37:40 -0500

    comment removal

--- AD_Users_Extract/AD_Users_Extract.Tests/UserServiceTests.cs
+++ AD_Users_Extract/AD_Users_Extract.Tests/UserServiceTests.cs
@@ -31,7 +31,6 @@ namespace AD_Users_Extract.Tests
 
             // ACT
             var _ = await unitUnderTest.GetUsers(groupId, token, syncDurationInHours);
-            // fail
         }
 
         [TestMethod]
