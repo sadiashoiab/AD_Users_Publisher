35ad715bc2249bdeb9d3ccdfa0d54aa758727668
Jamie Wallingford <jamie.wallingford@capstonec.com>
2019-11-04 12:51:24 -0600

    initial stubbing in of Salesforce TimeZone Service Logic

---
+++ Azure_AD_Users_Publisher/Azure_AD_Users_Publisher.Services/Models/SalesforceSupportedTimeZone.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Azure_AD_Users_Publisher.Services.Models
+{
+    [ExcludeFromCodeCoverage]
+    public struct SalesforceSupportedTimeZone
+    {
+        public SalesforceSupportedTimeZone(int gmtOffset, string timeZoneName, string timeZoneId)
+        {
+            TimeZoneOffset = gmtOffset;
+            TimeZoneName = timeZoneName;
+            TimeZoneId = timeZoneId;
+        }
+
+        public int TimeZoneOffset { get; }
+        public string TimeZoneName { get; }
+        public string TimeZoneId { get; }
+    }
+}
---
+++ Azure_AD_Users_Publisher/Azure_AD_Users_Publisher.Services/SalesforceTimeZoneManagerService.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Azure_AD_Users_Publisher.Services.Models;
+using Azure_AD_Users_Shared.Exceptions;
+using LazyCache;
+using Microsoft.Extensions.Configuration;
+
+namespace Azure_AD_Users_Publisher.Services
+{
+    public class SalesforceTimeZoneManagerService : ISalesforceTimeZoneManagerService
+    {
+        private const string _cacheKeyPrefix = "_SalesforceTimeZoneManagerService_";
+
+        private readonly IAppCache _cache;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly string _timeZoneUrl;
+
+        public SalesforceTimeZoneManagerService(IAppCache cache, IConfiguration configuration, IHttpClientFactory httpClientFactory)
+        {
+            _cache = cache;
+            _httpClientFactory = httpClientFactory;
+            _timeZoneUrl = $"{configuration["SalesforceBaseUrl"]}{configuration["SalesforceTimeZoneUrl"]}";
+        }
+
+        public async Task<IList<SalesforceSupportedTimeZone>> GetSupportedTimeZones()
+        {
+            var salesforceTimeZones = await _cache.GetOrAddAsync(_cacheKeyPrefix, RetrieveSalesforceTimeZones);
+            return salesforceTimeZones;
+        }
+
+
+        private async Task<IList<SalesforceSupportedTimeZone>> RetrieveSalesforceTimeZones()
+        {
+            var responseMessage = await SendAsync(_timeZoneUrl);
+            var json = await responseMessage.Content.ReadAsStringAsync();
+            // todo: use correct type
+            var results = System.Text.Json.JsonSerializer.Deserialize<IList<SalesforceSupportedTimeZone>>(json);
+            return results;
+        }
+
+        private async Task<HttpResponseMessage> SendAsync(string url)
+        {
+            var client = _httpClientFactory.CreateClient("SalesforceHttpClient");
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+            var responseMessage = await client.SendAsync(requestMessage);
+            if (!responseMessage.IsSuccessStatusCode)
+            {
+                throw new UnexpectedStatusCodeException(responseMessage);
+            }
+
+            if (responseMessage.Content == null)
+            {
+                throw new UnexpectedDataException("No results from Salesforce TimeZone Manager");
+            }
+
+            return responseMessage;
+        }
+    }
+}
